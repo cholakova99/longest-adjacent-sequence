0f36c7f2210759a9d4bb532d96367bd3e299f638
Zlatina Cholakova <zlatinatc99@gmail.com>
2020-10-26 11:35:27 +0200

    changes in tests and main.py

--- logic/create_matrix.py
+++ logic/create_matrix.py
@@ -5,14 +5,24 @@ def create_matrix_from_file(file_name):
         list_with_info[i] = list_with_info[i][:-1]
     first_line = list_with_info[0].split()
     rows = int(first_line[0])
-    cols = int(first_line[1])
+    # cols = int(first_line[1])
     created_matrix = []
     helper = []
-    for i in range(rows+1):
+    for i in range(rows + 1):
         helper = list_with_info[i].split()
-        if i != 0 and len(helper)  != cols:
-            raise ValueError('Wrong input!')
         created_matrix.append(helper)
         helper = []
     created_matrix = created_matrix[1:]
+    if check_matrix_correct(created_matrix, len(created_matrix), len(created_matrix[0])) is not True:
+        raise ValueError('Wrong input!')
     return created_matrix
+
+
+def check_matrix_correct(matrix, rows, cols):
+    for r in range(rows):
+        if len(matrix[r]) != cols:
+            raise ValueError('Wrong input!')
+        for c in range(cols):
+            if matrix[r][c] != 'R' and matrix[r][c] != 'G' and matrix[r][c] != 'B':
+                raise ValueError('Wrong input!')
+    return True
--- play_tests/test.py
+++ play_tests/test.py
@@ -5,16 +5,19 @@ sys.path.append('.')
 from logic import *
 from testfixtures import should_raise
 
-
 class TestLongestSeq(unittest.TestCase):
     def test_correct_created(self):
         created_matrix = create_matrix_from_file(f'tests/test_1')
-        expected = [["R","R","B"],["G","G","R"],["R","B","G"]]
+        expected = [["R", "R", "B"], ["G", "G", "R"], ["R", "B", "G"]]
         self.assertEqual(created_matrix, expected)
 
     @should_raise(ValueError)
     def test_wrong_input(self):
         created_matrix = create_matrix_from_file(f'tests/test_wrong_input')
+
+    @should_raise(ValueError)
+    def test_wrong_letter_input(self):
+        created_matrix = create_matrix_from_file(f'tests/test_wrong_input_2')
 
 
 if __name__ == '__main__':
